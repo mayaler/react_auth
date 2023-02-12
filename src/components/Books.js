f83e5970354b9d0248d6264a7ecdf7ab97a6499d
mayaaaal <m.lerman@interface3.be>
2023-02-12 23:03:44 +0100

    table

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Books() {
-  const [books, setBooks] = useState("");
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const configuration = {
@@ -25,18 +25,22 @@ export default function Books() {
       <h1> Example of React Map Loop </h1>
 
       <table className='table table-bordered'>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Author</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+          </tr>
+        </thead>
 
         {books.map((book, index) => (
-          <tr data-index={index}>
-            <td>{book.id}</td>
-            <td>{book.title}</td>
-            <td>{book.author}</td>
-          </tr>
+          <tbody>
+            <tr data-index={index}>
+              <td>{book.id}</td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+            </tr>
+          </tbody>
         ))}
       </table>
     </div>
