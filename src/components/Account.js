ce06a27e4116743003ba80f0fbef1667864fde11
mayaler <lemayalerman@gmail.com>
2023-03-12 14:20:36 +0100

    git config mayaler

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import Login from "./Login";
 import Register from "./Register";
+//
 export default function Account() {
   return (
     <Row>
